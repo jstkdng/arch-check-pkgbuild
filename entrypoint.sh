88d764d512b7ebfe6e2b6be48424025d7ecb2631
jk <jk@vin.ovh>
2020-05-11 16:04:05 -0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/sh -l
 
+install_deps() {
+    # install make package dependencies
+    grep -E 'makedepends' PKGBUILD | \
+        sed -e 's/.*depends=//' -e 's/ /\n/g' | \
+        tr -d "'" | tr -d "(" | tr -d ")" | \
+        xargs yay -S --noconfirm
+}
+
 # refresh pacman database beforehand
 sudo pacman -Syu --noconfirm
 
 sudo chown -R build /github/workspace /github/home
 
 cd $GITHUB_WORKSPACE
-makepkg --syncdeps --nobuild --noconfirm
+install_deps
+makepkg --nobuild --nodeps
 
 ret=$?
 if [ $ret -ne 0 ]; then
