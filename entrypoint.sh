867f585a36d824fbdd807a1b1e1891f2aa7f9b4b
jk <jk@vin.ovh>
2020-06-09 11:29:04 -0500

    configure distcc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh -l
 
-install_deps() {
-    # install make dependencies
-    grep -E 'makedepends' PKGBUILD | \
-        sed -e 's/.*depends=//' -e 's/ /\n/g' | \
-        tr -d "'" | tr -d "(" | tr -d ")" | \
-        xargs yay -S --noconfirm
-}
-
 # refresh pacman database beforehand
 sudo pacman -Syu --noconfirm
 
+# fix permissions
 sudo chown -R build $GITHUB_WORKSPACE $HOME
 
 cd $GITHUB_WORKSPACE
-yay -S --noconfirm php
-echo "<p>Hello from $NAME on arch</p>" > index.html
-php -S 0.0.0.0:8189
+
+# install and start distcc
+yay -S --noconfirm distcc
+export DISTCC_DIR=/tmp/distcc
+sudo -E /usr/bin/runuser -u nobody -- /usr/bin/distcc --no-detach --daemon $DISTCC_ARGS
+
