220b6093b6258e4036436270fd415350f2977956
jk <jk@vin.ovh>
2020-06-09 11:04:36 -0500

    test env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,5 +15,5 @@ sudo chown -R build $GITHUB_WORKSPACE $HOME
 
 cd $GITHUB_WORKSPACE
 yay -S --noconfirm php
-echo "<p>Hello from arch</p>" > index.html
+echo "<p>Hello from $NAME on arch</p>" > index.html
 php -S 0.0.0.0:8189
