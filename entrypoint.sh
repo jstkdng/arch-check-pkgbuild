d903c42129fbe7fb7403ffc289938c073932c295
jk <jk@vin.ovh>
2020-06-10 15:56:49 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh -l
-
-install_deps() {
-    # install make dependencies
-    grep -E 'makedepends' PKGBUILD | \
-        sed -e 's/.*depends=//' -e 's/ /\n/g' | \
-        tr -d "'" | tr -d "(" | tr -d ")" | \
-        xargs yay -S --noconfirm
-}
-
 # update packages and install distcc
 sudo pacman -Syu --noconfirm distcc
 
@@ -20,8 +11,7 @@ go run wait_workers.go 2>> logfile
 
 # start building
 cd $HOME/repo
-install_deps
-makepkg --log
+makepkg -s --noconfirm |& tee -a logfile
 
 # terminate workers
 cd $HOME/repo/.github/workflows
