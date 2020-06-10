5b94464a9e6a001c0ba438508c37d3f30b4eb4a1
jk <jk@vin.ovh>
2020-06-10 18:50:20 -0500

    update entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -l
 
+# wait for workers
+go run .github/workflows/wait_workers.go
+
 # switch to canada mirror
 sudo echo "Server = https://mirror.scd31.com/arch/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist
+
+# update packages and install distcc
+sudo pacman -Syu --noconfirm distcc
 
 # fix permissions
 sudo chown -R build $HOME
@@ -9,14 +15,8 @@ sudo chown -R build $HOME
 # switch to repo
 cd $HOME/repo
 
-# update packages and install distcc
-sudo pacman -Syu --noconfirm distcc |& tee -a logfile
-
-# wait for workers
-go run .github/workflows/wait_workers.go 2>> logfile
-
 # start building
 makepkg -s --noconfirm |& tee -a logfile
 
 # terminate workers
-go run .github/workflows/end_workers.go 2>> logfile
+go run .github/workflows/end_workers.go
