bd7d5134621694f2298f053743a684d253d3895f
jk <jk@vin.ovh>
2020-06-10 16:22:59 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
-# refresh pacman database beforehand
-sudo pacman -Syu --noconfirm
+# install required dependencies
+sudo pacman -Syu --noconfirm clang distcc
 
 # fix permissions
 sudo chown -R build $HOME
 
 # install and start distcc
-yay -S --noconfirm distcc
-sudo /usr/bin/runuser -u nobody -- /usr/bin/distccd --no-detach --daemon $DISTCC_ARGS
+export DISTCC_DIR=/tmp/distcc
+sudo -E /usr/bin/runuser -u nobody -- /usr/bin/distccd --no-detach --daemon $DISTCC_ARGS
 
