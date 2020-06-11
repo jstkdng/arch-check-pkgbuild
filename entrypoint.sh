763c5ddc5bcf2318afde179f276df735c2f6219f
jk <jk@vin.ovh>
2020-06-10 20:58:35 -0500

    simplify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
-shopt -s dotglob
-
 install_deps() {
     # install make dependencies
     grep -E 'makedepends' PKGBUILD | \
@@ -10,22 +8,19 @@ install_deps() {
 }
 
 # switch to canada mirror
-sudo echo "Server = http://archlinux.mirror.colo-serv.net/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist
+echo "Server = http://archlinux.mirror.colo-serv.net/\$repo/os/\$arch" | sudo tee /etc/pacman.d/mirrorlist
+
+# update packages and install distcc
+sudo pacman -Syu --noconfirm distcc
+
+# change permissions
+sleep 30
+sudo chown -R build $HOME
 
 # switch to repo
 cd $HOME/repo
 
-# wait for the PKGBUILD
-while ! test -f "PKGBUILD" do
-    sleep 2
-done
-sleep 10 # wait a bit more just in case
-
-# change permissions
-sudo chown -R build:build $HOME
-
-# update packages and install distcc
-sudo pacman -Syu --noconfirm distcc
+echo "Starting build..." > logfile
 
 # wait for workers
 go run .github/workflows/wait_workers.go |& tee -a logfile
